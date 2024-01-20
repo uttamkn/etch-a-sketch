f10c930e29283cab5950f177a29d512cef99a059
uttamkn <uttamkn15@gmail.com>
2024-01-20 19:34:27 +0530

    Create a dynamic grid based on the user input and change the pixel color when dragged upon

--- script.js
+++ script.js
@@ -4,15 +4,34 @@ const options = document.createElement('div');
 const grid = document.createElement('div');
 const inputContainer = document.createElement('div');
 const inputWrapper = document.createElement('div');
+const userInputBox = document.createElement('input');
+const inputBtn = document.createElement('button');
 
 container.classList.add('container');
 optAndGrid.classList.add('optAndGrid');
 options.classList.add('options');
 grid.classList.add('grid');
 inputContainer.classList.add('inputContainer');
+inputWrapper.classList.add('inputWrapper')
+userInputBox.setAttribute('type', 'number');
+userInputBox.setAttribute('id', 'userInput');
+inputBtn.classList.add('inputBtn');
+inputBtn.innerText='enter';
+
+optAndGrid.appendChild(options);
+optAndGrid.appendChild(grid);
+
+inputWrapper.appendChild(userInputBox);
+inputWrapper.appendChild(inputBtn);
+inputContainer.appendChild(inputWrapper);
+
+container.appendChild(optAndGrid);
+container.appendChild(inputContainer);
+
+document.querySelector('body').appendChild(container);
 
 function addPixels(dimensions) {           //Adds pixels to the grid based on the given dimensions
-    let pixelHeight = 360/dimensions;
+    let pixelHeight = 280/dimensions;
     
     for (let i=0; i<(dimensions*dimensions); i++) {             
         const pixel = document.createElement('div');
@@ -21,18 +40,44 @@ function addPixels(dimensions) {           //Adds pixels to the grid based on th
         pixel.style.width=`${pixelHeight}px`
         grid.appendChild(pixel);
     }
+    dragEffect();
 }
-addPixels(16)
 
-optAndGrid.appendChild(options);
-optAndGrid.appendChild(grid);
+function removePixels() {                   //Removes all the pixels from grid
+    const pixels = document.querySelectorAll('.pixels');
+    pixels.forEach((pixel) => {
+        grid.removeChild(pixel);
+    });
+}
 
-inputContainer.appendChild(inputWrapper);
+function dragEffect(){
+    const pixels = document.querySelectorAll('.pixels');
+    pixels.forEach((pixel) => {
+        pixel.addEventListener('dragenter', () => {
+            pixel.style.background='black';
+        });
+        pixel.addEventListener('mousedown', () => {
+            pixel.style.background='black';
+        });
+    });
+}
 
-container.appendChild(optAndGrid);
-container.appendChild(inputContainer);
+addPixels(16);                               //default grid
 
-document.querySelector('body').appendChild(container);
+inputBtn.addEventListener('click', () => {
+    let userInput = Number(userInputBox.value);
+
+    if(userInput>0 && userInput <=100) {
+        removePixels();                     //to change the dimensions of grid
+        addPixels(userInput);
+    }
+    else {
+        alert("ERROR: Input a number between 1 and 100");
+    }
+});
+
+
+
 
 
 
