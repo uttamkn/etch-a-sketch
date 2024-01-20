1c453e485778cb5a698a3d0e346099f04654bcea
uttamkn <uttamkn15@gmail.com>
2024-01-20 15:06:21 +0530

    create elements for viewport

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.createElement('div');
+const optAndGrid = document.createElement('div'); 
+const options = document.createElement('div');
+const grid = document.createElement('div');
+const inputContainer = document.createElement('div');
+
